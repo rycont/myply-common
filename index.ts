f20888b9e46cdaa1254cc1c8c2c76abf0a8a72f6
RyCont(정한) <35295182+rycont@users.noreply.github.com>
2021-08-06 23:35:25 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
 export class PlyError {
     channel: string;
     message: string;
+    constructor(channel: string, message: string) {
+        this.channel = channel;
+        this.message = message;
+    }
 }
 
 export interface Song {
     title: string;
     artist: string;
-    id: Record<string, string>
+    id: Record<string, string>;
 }
 
 export interface Adaptor {
