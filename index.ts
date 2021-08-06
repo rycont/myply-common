0d779c8a4a195ca8f4b7bc97fba7a8a6c7390496
rycont <reactdev@kakao.com>
2021-08-06 23:19:27 +0900

    init

---
+++ index.ts
@@ -0,0 +1,18 @@
+export class PlyError {
+    channel: string;
+    message: string;
+}
+
+export interface Song {
+    title: string;
+    artist: string;
+    id: {
+        melon: string;
+    }
+}
+
+export interface Adaptor {
+    getPlaylistContent(url: string): Promise<Song[] | PlyError>;
+    generateURL(keys: string[]): Promise<string>;
+    findSongId(song: Song): Promise<string>;
+}
